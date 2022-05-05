6f380182a14a133b3403cc292b853f419995daaf
EC2 Default User <ec2-user@ip-172-31-41-151.ap-northeast-1.compute.internal>
2022-05-05 17:27:48 +0000

    devise_install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # サインイン後遷移画面
+  def after_sign_in_path(resource)
+    root_path
+  end
+
+  #サインアウト後遷移画面
+  def after_sign_out_path(resource)
+    new_user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+  end
 end
