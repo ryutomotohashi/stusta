659b78585f6e3bc73aabd7abfe81a31762b2f111
EC2 Default User <ec2-user@ip-172-31-41-151.ap-northeast-1.compute.internal>
2022-05-06 05:18:54 +0000

    Active_Storage_install

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :image
+
+
+  #get_imageで画像取得時、画像が無かった場合"no-image.jpg"を表示
+  def get_image(width,height)
+    unless image.attached?
+        file_path = Rails.root.join("app/assets/images/no_image.jpg")
+        image.attach(io:File.open(file_path),filename: "default-image.jpg",content_type: "image/jpeg")
+    end
+    image.variant(resize_to_limit:[width, height]).processed
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.active_job.queue_adapter = :inline
 
   config.hosts << "4e82ffcd63824f13b9d190b37292fca4.vfs.cloud9.ap-northeast-1.amazonaws.com"
 
